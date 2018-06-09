86706ea45076653a7650490b772fa9059f4e6111
syafiqm7 <39830823+syafiqm7@users.noreply.github.com>
2018-06-10 06:21:20 +0800

    Update t ban.sh

--- t ban.sh
+++ t ban.sh
@@ -7,16 +7,16 @@ apt update
 apt upgrade
 apt install figlet -y
 figlet Basic Installation
-apt install toilet -y
 apt install cowsay -y
 apt install nano -y
 apt install ruby -y
+apt install screenfetch -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
 echo -e "\e[1m Now Run \e[32mbash t-ban.sh\e[0m...!!!"
 echo
-echo -e "\e[1m\e[32m Developed by :\e[33m XANDER (8h4i)"
+echo -e "\e[1m\e[32m Developed by :\e[33m XANDER "
 echo
 echo
 clear 

34742d0876a6868e6361ec82682042ae8b4a2bec
syafiqm7 <39830823+syafiqm7@users.noreply.github.com>
2018-06-10 21:52:03 +0800

    Update t-ban.sh

--- t-ban.sh
+++ t-ban.sh
@@ -6,15 +6,15 @@ clear
 apt update
 apt upgrade
 apt install figlet -y
-figlet Basic Installation
+figlet Termux Banner |lolcat
 apt install cowsay -y
 apt install nano -y
 apt install ruby -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
-echo -e "\e[1m Now Run \e[32mbash t-ban.sh\e[0m...!!!"
-echo
+echo -e "\e[1m Now Run \e[32mbash "t-ban.sh"\e[0m...!!!"
+echo -e $yellow "\e[1m developed by:syafiqm7
 echo 
 echo
 echo
